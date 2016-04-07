43ffae23c8a87c161e8a8b7498365d43d5d843f0
liuguangxuan <guangxuanliu@gmail.com>
2016-04-07 10:53:10 +0800

    第20章结束

---
+++ Scripts/Class20.js
@@ -0,0 +1,23 @@
+/**
+ * Created by 技术2 on 2016/4/7.
+ */
+var book = {
+    title : "Professional JavaScript",
+    authors :[
+        "Nicholas C. Zakas"
+    ],
+    edition : "3",
+    year : 2011,
+    releaseDate : new Date(2011,11,1)
+};
+var jsonText = JSON.stringify(book);
+var bookCopy = JSON.parse(jsonText,function (key,value) {
+    if(key == "releaseDate"){
+        return new Date(value);
+    }
+    else {
+        return value;
+    }
+});
+console.log(book);
+console.log(bookCopy);
